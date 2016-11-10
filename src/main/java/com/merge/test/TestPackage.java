7f8294ae376b331b87d19f003bb4dd16f508a144
Sujay <sujaya@cybage.com>
2016-11-10 11:09:15 +0530

    added calculate function

--- src/main/java/com/merge/test/TestPackage.java
+++ src/main/java/com/merge/test/TestPackage.java
@@ -4,5 +4,10 @@ public class TestPackage {
 
 	public static void main(String[] s) {
 		System.out.println("Test");
+		calculate(4, 5);
+	}
+	
+	public static void calculate(int a, int b) {
+		System.out.println(a+b);
 	}
 }
