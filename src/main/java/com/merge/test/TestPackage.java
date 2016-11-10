aaf3b40453a25003e94000be30c961cb5e979c6f
Sujay <sujaya@cybage.com>
2016-11-10 10:58:22 +0530

    Updated with display function

--- src/main/java/com/merge/test/TestPackage.java
+++ src/main/java/com/merge/test/TestPackage.java
@@ -4,5 +4,10 @@ public class TestPackage {
 
 	public static void main(String[] s) {
 		System.out.println("Test");
+		display();
+	}
+	
+	public static void display() {
+		System.out.println("Display");
 	}
 }
