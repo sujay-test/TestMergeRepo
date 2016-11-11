24ab10defaed8c78d200e6ef7a83ec10ad98645e
Sujay <sujaya@cybage.com>
2016-11-11 15:10:21 +0530

    Added string parameter

--- src/main/java/com/merge/test/TestPackage.java
+++ src/main/java/com/merge/test/TestPackage.java
@@ -2,7 +2,13 @@ package com.merge.test;
 
 public class TestPackage {
 
+	private String string;
+
 	public static void main(String[] s) {
 		System.out.println("Test");
+	}
+	
+	public void getString() {
+		return string;
 	}
 }
